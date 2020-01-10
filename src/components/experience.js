bd493543800842e253fcc7fc29db9c7952376bae
Jonny Stoten <jonny@jonnystoten.com>
2020-01-10 21:07:13 +0000

    Remove page break

--- src/components/experience.js
+++ src/components/experience.js
@@ -24,7 +24,6 @@ export const Experience = ({ data }) => (
             <MarkdownHtml markdown={node.projects} />
           </div>
         )}
-        {index === 0 && <PrintPageBreak />}
       </div>
     ))}
   </div>
